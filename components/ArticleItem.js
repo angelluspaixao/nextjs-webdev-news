8ff083b083c6af2d206eca9fdd1e69e6694a5bae
Angellus <paixao.angellus@gmail.com>
2022-12-17 19:38:45 -0300

    Add Article component estructure

---
+++ components/ArticleItem.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from '../styles/Article.module.css'
+
+const ArticleItem = ({ article }) => {
+    return (
+        <Link href='/article/[id]' as={`/article/${article.id}`} className={styles.card}>
+                <h3>{article.title} &rarr;</h3>
+                <p>{article.body}</p>
+        </Link>
+    )
+}
+
+export default ArticleItem
