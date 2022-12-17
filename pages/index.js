b9f7061661dc6ffa10ccd3f0f53f35c93f42d245
Angellus <paixao.angellus@gmail.com>
2022-12-17 19:39:16 -0300

    Update using Article components and path generate

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch(`https:jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
+}
+
+export default function Home({ articles }) {
   return (
     <>
       <Head>
         <title>WebDev News</title>
         <meta name='keywords' content='web deelopment, programing, next.js, nextjs' />
       </Head>
-      <h1>Welcome to Netx.js</h1>
+      <ArticleList articles={articles}/>
     </>
   )
 }
